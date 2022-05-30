4dfcc445975a5515734fe2c7ce5481986a15dadd
wanglongjs <daiwanglong1@163.com>
2022-05-30 17:12:11 +0800

    提交

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router';
+import router from './router'
 const app = createApp(App)
+const a = 3
+console.log(123)
 app.use(router)
 app.mount('#app')
